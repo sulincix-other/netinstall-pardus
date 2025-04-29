7c0f98826cba587973bc58004623efde4703e565
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-04-29 12:19:45 +0300

    init fix

--- init.sh
+++ init.sh
@@ -6,8 +6,9 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 mount -t devtmpfs devtmpfs /dev
 ############### udevd ###############
-/lib/systemd/systemd-udevd --daemon --debug > /var/log/udevd.log
-udevadm trigger -c add
+/lib/systemd/systemd-udevd --daemon --debug &> /var/log/udevd.log
+udevadm trigger -c add &> /var/log/udevd-trigger.log
+udevadm settle
 sync && sleep 1
 
 ############### networking ###############
@@ -20,8 +21,8 @@ if [ "\$router" ]; then
 fi
 EOF
 for dev in $(ls /sys/class/net/ | grep -v lo) ; do
-    busybox ip link set up $dev
-    busybox udhcpc -i $dev
+    busybox ip link set up $dev || true
+    busybox udhcpc -i $dev -s /usr/share/udhcpc/default.script || true
 done
 sync && sleep 1
 
@@ -47,32 +48,32 @@ fi
 
 mkdir -p /target
 if [ -d /sys/firmware/efi ] ; then
-    parted -s /dev/"${DISK}" mktable gpt
-    parted -s /dev/"${DISK}" mkpart primary fat32 1 "500MB"
-    parted -s /dev/"${DISK}" mkpart primary fat32 500MB "100%"
+    parted -s /dev/"${DISK}" mktable gpt || bash
+    parted -s /dev/"${DISK}" mkpart primary fat32 1 "500MB" || bash
+    parted -s /dev/"${DISK}" mkpart primary fat32 500MB "100%" || bash
     sync && sleep 1
-    yes | mkfs.vfat /dev/${DISKX}1
-    yes | mkfs.ext4  /dev/${DISKX}2
+    yes | mkfs.vfat /dev/${DISKX}1 || bash
+    yes | mkfs.ext4  /dev/${DISKX}2 || bash
     mount /dev/${DISKX}2 /target
     mkdir -p /target/boot/efi
-    mount /dev/${DISKX}2 /target/boot/efi
+    mount /dev/${DISKX}2 /target/boot/efi || bash
     uuid_efi=$(blkid  /dev/${DISKX}1 | sed "s/ /\n/g" | grep "^UUID")
     uuid_rootfs=$(blkid  /dev/${DISKX}2 | sed "s/ /\n/g" | grep "^UUID")
     echo "${uuid_efi} /boot/efi vfat defaults,rw 0 0" > /tmp/fstab
     echo "${uuid_rootfs} / ext4 defaults,rw 0 1" >> /tmp/fstab
 else
-    parted -s /dev/"${DISK}" mktable msdos
-    parted -s /dev/"${DISK}" mkpart primary fat32 1 "100%"
+    parted -s /dev/"${DISK}" mktable msdos || bash
+    parted -s /dev/"${DISK}" mkpart primary fat32 1 "100%" || bash
     sync && sleep 1
-    yes | mkfs.ext4  /dev/${DISKX}1
-    mount /dev/${DISKX}1 /target
+    yes | mkfs.ext4  /dev/${DISKX}1 || bash
+    mount /dev/${DISKX}1 /target || bash
     uuid_rootfs=$(blkid  /dev/${DISKX}1 | sed "s/ /\n/g" | grep "^UUID")
     echo "${uuid_rootfs} / ext4 defaults,rw 0 1" >> /tmp/fstab
 fi
 sync && sleep 1
 
 ############### install base system ###############
-debootstrap --include "usr-is-merged usrmerge" /target work/chroot https://depo.pardus.org.tr/pardus
+debootstrap --include "usr-is-merged usrmerge" yirmiuc-deb /target https://depo.pardus.org.tr/pardus || bash
 cat > /target/etc/apt/sources.list <<EOF
 ### The Official Pardus Package Repositories ###
 
@@ -90,10 +91,10 @@ deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-fre
 
 EOF
 
-chroot /target apt update --allow-insecure-repositories
-chroot /target apt install pardus-archive-keyring --allow-unauthenticated -yq
-chroot /target apt update
-chroot /target apt full-upgrade -yq
+chroot /target apt update --allow-insecure-repositories || bash
+chroot /target apt install pardus-archive-keyring --allow-unauthenticated -yq || bash
+chroot /target apt update || bash
+chroot /target apt full-upgrade -yq || bash
 sync && sleep 1
 
 ############### install packages ###############
@@ -101,20 +102,20 @@ for dir in dev sys proc ; do
     mount --bind /$dir /target/$dir
 done
 # kernel
-chroot /target apt install -yq linux-image-amd64
+chroot /target apt install -yq linux-image-amd64 || bash
 # desktop
-chroot /target apt install -yq pardus-xfce-desktop
+chroot /target apt install -yq pardus-xfce-desktop || bash
 sync && sleep 1
 
 ############### install grub ###############
 mv /tmp/fstab /target/etc/fstab
 if [ -d /sys/firmware/efi ] ; then
-    chroot /target apt install -yq grub-efi
-    chroot /target mount -t efivarfs efivarfs /sys/firmware/efi/efivars
-    chroot /target grub-install /dev/${DISK} --target="x86_64-efi"
+    chroot /target apt install -yq grub-efi || bash
+    chroot /target mount -t efivarfs efivarfs /sys/firmware/efi/efivars || bash
+    chroot /target grub-install /dev/${DISK} --target="x86_64-efi" || bash
 else
-    chroot /target apt install -yq grub-pc
-    chroot /target grub-install /dev/${DISK} --target="i386-pc"
+    chroot /target apt install -yq grub-pc || bash
+    chroot /target grub-install /dev/${DISK} --target="i386-pc" || bash
 fi
 sync && sleep 1
 
@@ -139,7 +140,7 @@ echo "Europe/Istanbul" > /target/etc/timezone
 chroot /target timedatectl set-timezone Europe/Istanbul || true
 rm -f /target/etc/localtime  || true
 ln -s ../usr/share/zoneinfo/Turkey /target/etc/localtime
-chroot /target locale-gen
+chroot /target locale-gen || true
 
 # Hosts file
 echo "pardus" > /target/etc/hostname
@@ -151,7 +152,7 @@ echo "::1     localhost ip6-localhost ip6-loopback" >> /target/etc/hosts
 echo "ff02::1 ip6-allnodes" >> /target/etc/hosts
 echo "ff02::2 ip6-allrouters" >> /target/etc/hosts
 
-chroot /target useradd -m pardus -c "Pardus" -G cdrom,floppy,sudo,audio,dip,video,plugdev,netdev,bluetooth,scanner,lpadmin -s /bin/bash -p $(openssl passwd -6 pardus)
+chroot /target useradd -m pardus -c "Pardus" -G cdrom,floppy,sudo,audio,dip,video,plugdev,netdev,bluetooth,scanner,lpadmin -s /bin/bash -p $(openssl passwd -6 pardus) || bash
 
 sync && sleep 1
 
